bd8fb74c53acd02b91c7e12d399f0aa8b9e5f5d3
Ernest <emuroiwa@gmail.com>
2020-04-29 19:31:36 +0200

    update

--- routes/api/users.js
+++ routes/api/users.js
@@ -14,68 +14,59 @@ const userModel = require("../../models/User");
 //@desc     Test
 //@access   Public
 routes.post(
-  "/",
-  [
-    // name must be an string
-    check("name", "Name is required").notEmpty(),
-    //email
-    check("email").isEmail(),
-    // password must be at least 5 chars long
-    check("password").isLength({ min: 5 }),
-  ],
-  //callback fxn
-  async (request, response) => {
-    const errors = validationResult(request);
-    if (!errors.isEmpty()) {
-      return response.status(422).json({ errors: errors.array() });
-    }
+	"/",
+	[
+		// name must be an string
+		check("name", "Name is required").notEmpty(),
+		//email
+		check("email").isEmail(),
+		// password must be at least 5 chars long
+		check("password").isLength({ min: 5 }),
+	],
+	//callback fxn
+	async (request, response) => {
+		const errors = validationResult(request);
+		if (!errors.isEmpty()) {
+			return response.status(422).json({ errors: errors.array() });
+		}
 
-    const { name, email, password } = request.body;
+		const { name, email, password } = request.body;
 
-    try {
-      //check if user exisits
-      let user = await userModel.findOne({ email: email });
-      if (user) {
-        response.status(400).json({ errors: [{ msg: "user exists" }] });
-      }
-      //get avatar
-      const avatar = gravatar.url(email, {
-        s: "200",
-        r: "pg",
-        d: "mm",
-      });
+		try {
+			//check if user exisits
+			let user = await userModel.findOne({ email: email });
+			if (user) {
+				response.status(400).json({ errors: [{ msg: "user exists" }] });
+			}
+			//get avatar
+			const avatar = gravatar.url(email, {
+				s: "200",
+				r: "pg",
+				d: "mm",
+			});
 
-      user = new userModel({
-        name,
-        email,
-        avatar,
-        password,
-      });
+			user = new userModel({
+				name,
+				email,
+				avatar,
+				password,
+			});
 
-      const salt = await bcrypt.genSalt(10);
-      user.password = await bcrypt.hash(password, salt);
-      await user.save();
+			const salt = await bcrypt.genSalt(10);
+			user.password = await bcrypt.hash(password, salt);
+			await user.save();
 
-      const payload = {
-        user: {
-          id: user.id,
-        },
-      };
-      console.log(payload);
-      jwt.sign(
-        payload,
-        config.get("jwtSecret"),
-        { expiresIn: 360000 },
-        (err, token) => {
-          if (err) throw err;
-          res.json({ token });
-        }
-      );
-    } catch (error) {
-      console.error(error);
-      response.status(500).send("Failed");
-    }
-  }
+			const payload = {
+				user: {
+					id: user.id,
+				},
+			};
+			response.status(200).send("Success");
+		} catch (error) {
+			console.error(error);
+			response.status(500).send("Failed");
+		}
+	}
 );
 
 module.exports = routes;
