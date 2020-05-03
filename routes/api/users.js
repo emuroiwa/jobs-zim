73e8229d19361049f844c44050a40a46cfc39579
Ernest <emuroiwa@gmail.com>
2020-05-03 09:51:40 +0200

    login

--- routes/api/users.js
+++ routes/api/users.js
@@ -5,61 +5,75 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const config = require("config");
 const routes = express.Router();
+
 // ...rest of the initial code omitted for simplicity.
 const { check, validationResult } = require("express-validator");
-
 const userModel = require("../../models/User");
 
 //@route    GET api/users
 //@desc     Test
 //@access   Public
 routes.post(
-	"/",
-	//validation middleware
-	[
-		check("name", "Name is required").notEmpty(),
-		check("email").isEmail(),
-		check("password").isLength({ min: 5 }),
-	],
-	//callback fxn
-	async (request, response) => {
-		const errors = validationResult(request);
-		if (!errors.isEmpty()) {
-			return response.status(422).json({ errors: errors.array() });
-		}
+  "/",
+  //validation middleware
+  [
+    check("name", "Name is required").notEmpty(),
+    check("email").isEmail(),
+    check("password").isLength({ min: 5 }),
+  ],
+  //callback fxn
+  async (request, response) => {
+    const errors = validationResult(request);
+    if (!errors.isEmpty()) {
+      return response.status(422).json({ errors: errors.array() });
+    }
 
-		const { name, email, password } = request.body;
+    const { name, email, password } = request.body;
 
-		try {
-			//check if user exisits
-			let user = await userModel.findOne({ email: email });
-			if (user) {
-				response.status(400).json({ errors: [{ msg: "user exists" }] });
-			}
-			//get avatar
-			const avatar = gravatar.url(email, {
-				s: "200",
-				r: "pg",
-				d: "mm",
-			});
+    try {
+      //check if user exisits
+      let user = await userModel.findOne({ email: email });
+      if (user) {
+        response.status(400).json({ errors: [{ msg: "user exists" }] });
+      }
+      //get avatar
+      const avatar = gravatar.url(email, {
+        s: "200",
+        r: "pg",
+        d: "mm",
+      });
 
-			user = new userModel({
-				name,
-				email,
-				avatar,
-				password,
-			});
+      user = new userModel({
+        name,
+        email,
+        avatar,
+        password,
+      });
 
-			const salt = await bcrypt.genSalt(10);
-			user.password = await bcrypt.hash(password, salt);
-			await user.save();
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+      await user.save();
 
-			response.status(200).send("Success");
-		} catch (error) {
-			console.error(error);
-			response.status(500).send("Failed");
-		}
-	}
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          response.json({ token });
+        }
+      );
+    } catch (error) {
+      console.error(error);
+      response.status(500).send("Failed");
+    }
+  }
 );
 
 module.exports = routes;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const connectDB = require("./config/db");
 
+//calling the constructor
 const app = express();
 //connect to db
 connectDB();
@@ -12,6 +13,7 @@ app.get("/", (req, res) => res.send("Jobs-Zim"));
 
 //define routes
 app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
 
 const PORT = process.env.PORT || 5000;
 
