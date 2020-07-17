a5b0169cff6c02c1b8038dd2ee02fc015bff5797
Ernest <emuroiwa@gmail.com>
2020-07-17 12:49:55 +0200

    payfast

--- server.js
+++ server.js
@@ -16,7 +16,8 @@ app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
+app.use("/api/payfast", require("./routes/api/payfast"));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8081;
 
 app.listen(PORT, () => console.log(`Server on ${PORT}`));
